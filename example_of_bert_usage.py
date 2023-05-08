3a8f93ecb5782550082180e26ef7a67c00aa318f
DZIMDZEM <132267102+DZIMDZEM@users.noreply.github.com>
2023-05-08 19:28:23 +0200

    Update example_of_bert_usage.py

--- example_of_bert_usage.py
+++ example_of_bert_usage.py
@@ -12,3 +12,17 @@ pipeline = Pipeline([
     ("preprocessing", DataframePreprocessor(preprocessing)),
     ("vectorizing", BertVectorizer(tokenizer))
 ])
+
+import pandas as pd
+
+data_sample = pd.DataFrame({
+    'tweet': [
+        'Sasha goes to Mannheim University',
+        'Danylo will create a great application',
+        'Danylo will not create a great application',
+        'Dasha didnt fulfill her task'
+    ],
+    'class': [1, 1, 0, 0]
+})
+
+pipeline.transform(data_sample)
