23723386ec49f1e6cbb33f9a877c01197d60541b
DZIMDZEM <132267102+DZIMDZEM@users.noreply.github.com>
2023-05-15 22:22:18 +0200

    Update bert_processor.py

--- bert_processor.py
+++ bert_processor.py
@@ -1,12 +1,12 @@
 import html
 import re
+from typing import Callable
 
 import nltk
 import pandas as pd
 import torch
 from emot.emo_unicode import UNICODE_EMOJI  # For emojis
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet, stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from sklearn.base import TransformerMixin, BaseEstimator
@@ -20,7 +20,7 @@ tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 
 
 # class for preprocessing
-class DataframePreprocessor(BaseEstimator, TransformerMixin):
+class DataCleaner(BaseEstimator, TransformerMixin):
     def __init__(self, func: callable):
         self.func = func
 
@@ -29,104 +29,101 @@ class DataframePreprocessor(BaseEstimator, TransformerMixin):
 
     def transform(self, df: pd.DataFrame, y=None, **transform_params):
         _df = df.copy()
-        _df["tweet"], _df["rt_flag"] = zip(*df["tweet"].transform(lambda x: self.func(x)))
+        _df["tweet"] = _df["tweet"].transform(lambda x: self.func(x))
         return _df
 
 
-# Lemmatization
+class TextCleaner(TransformerMixin, BaseEstimator):
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('wordnet')
 
-nltk.download('stopwords')
-nltk.download('punkt')
-
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-
-stop_words = set(stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
-
-
-def get_wordnet_pos(treebank_tag):
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return wordnet.NOUN
-
-
-# Functions for extracting 'rt_flag' feature
-def annotate_rt(text):
-    return 1 if len(text.split()) > 0 and text.split()[0] == 'rt' else 0
-
-
-def delete_rt_info(text):
-    if len(text) > 0:
-        return text if text.split()[0] != 'rt' else " ".join(text.split()[1:])
-    return text
-
-
-# Function for converting emojis into word
-def convert_emojis(text):
-    text = html.unescape(text)
-    for emot in UNICODE_EMOJI:
-        text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",", "").replace(":", "").split()))
-    return text
-
-
-def lemmatize(text):
-    tokens = nltk.word_tokenize(text)
-    tagged = nltk.pos_tag(tokens)
-    lemmatized = [lemmatizer.lemmatize(word, get_wordnet_pos(pos_tag)) for (word, pos_tag) in tagged]
-
-    return " ".join(lemmatized)
-
-
-def remove_stopwords(tweet):
-    tokens_tweet = word_tokenize(tweet)
-    tokens_without_sw = " ".join([word for word in tokens_tweet if not word in stop_words])
-
-    return tokens_without_sw
-
-
-# also adds a new column 'rt_flag'
-def preprocessing(tweet):
-    re_username = '@[\w\-]+'
-    re_exclamation = ""
+    re_username = r"@\w+"
     re_link = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
-               '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+               '[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
     re_all_words = "[^a-z ]+"
+    stop_words = set(stopwords.words('english'))
 
-    tweet = re.sub(re_username, "", tweet)
-    tweet = re.sub(re_link, "link", tweet)
-    tweet = tweet.lower()
-    tweet = convert_emojis(tweet)
-    tweet = re.sub(re_all_words, " ", tweet)
-    tweet = lemmatize(tweet)
-    tweet = remove_stopwords(tweet)
-    rt_flag = annotate_rt(tweet)
-    tweet = delete_rt_info(tweet)
+    lemmatizer = WordNetLemmatizer()
 
-    return tweet, rt_flag
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        return X \
+            .pipe(self.apply_to_tweet_column(self.delete_usernames)) \
+            .pipe(self.apply_to_tweet_column(self.delete_links)) \
+            .pipe(self.apply_to_tweet_column(self.convert_emojis)) \
+            .pipe(self.apply_to_tweet_column(self.to_lower)) \
+            .pipe(self.apply_to_tweet_column(self.delete_special_symbols)) \
+            .pipe(self.apply_to_tweet_column(self.lemmatize)) \
+            .pipe(self.apply_to_tweet_column(self.remove_stopwords))
+
+    def get_wordnet_pos(self, treebank_tag: str):
+        if treebank_tag.startswith('J'):
+            return wordnet.ADJ
+        elif treebank_tag.startswith('V'):
+            return wordnet.VERB
+        elif treebank_tag.startswith('N'):
+            return wordnet.NOUN
+        elif treebank_tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
+
+    @staticmethod
+    def convert_emojis(text: str) -> str:
+        text = html.unescape(text)
+        for emot in UNICODE_EMOJI:
+            text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",", "").replace(":", "").split()))
+        return text
+
+    def lemmatize(self, text: str) -> str:
+        lemmatized = [
+            self.lemmatizer.lemmatize(word, self.get_wordnet_pos(pos_tag))
+            for (word, pos_tag) in nltk.pos_tag(nltk.word_tokenize(text))
+        ]
+        return " ".join(lemmatized)
+
+    def remove_stopwords(self, tweet: str) -> str:
+        tokens_without_sw = " ".join(
+            [word for word in word_tokenize(tweet) if not word in [*self.stop_words, 'rt']]
+        )
+        return tokens_without_sw
+
+    def delete_usernames(self, text: str) -> str:
+        return re.sub(self.re_username, "username", text)
+
+    def delete_links(self, text: str) -> str:
+        return re.sub(self.re_link, "link", text)
+
+    def delete_special_symbols(self, text: str):
+        return re.sub(self.re_all_words, " ", text)
+
+    @staticmethod
+    def to_lower(text: str) -> str:
+        return text.lower()
+
+    def apply_to_tweet_column(self, f: Callable) -> Callable:
+        return lambda x: pd.DataFrame(x['tweet'].apply(f))
 
 
-class BertVectorizer(BaseEstimator, TransformerMixin):
-    def __init__(self, tokenizer: callable):
+class Vectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, tokenizer: callable, **kargs):
         self.model = BertModel.from_pretrained('bert-base-uncased')
         self.tokenizer = tokenizer
 
         self.config = {
             'add_special_tokens': True,
             'padding': 'max_length',
-            'max_length': 16,
+            'max_length': 32,
             'truncation': True,
             'pad_to_max_length': True,
             'return_attention_mask': False,
             'return_tensors': 'pt',
         }
+        self.config.update(kargs)
 
     def fit(self, df: pd.DataFrame, y=None, **fit_params):
         return self
